0b0bedd6067f15d3d8b354e580e8ce80133c8b0c
felixeliass1994 <felixeliass1994@gmail.com>
2024-08-05 16:24:06 +0900

    fix: bevm is added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -84,12 +84,27 @@ const config: HardhatUserConfig = {
             gasPrice: 1000000000, // 1 Gwei
             gasMultiplier: 2
         },
+        "ailayertestnet": {
+            url: "https://testnet-rpc.ailayer.xyz",
+            chainId: 2648,
+            accounts: [`${process.env.PRIVATE_KEY}`],
+            gasPrice: 1000000000, // 1 Gwei
+            gasMultiplier: 2
+        },
+        "bevmtestnet": {
+            url: "https://testnet.bevm.io",
+            chainId: 11503,
+            accounts: [`${process.env.PRIVATE_KEY}`],
+            gasPrice: 1000000000, // 1 Gwei
+            gasMultiplier: 2
+        },
     },
     etherscan: {
         apiKey: {
             arbitrumGoerli: `${process.env.ARBISCAN_API_KEY}`,
             arbitrumOne: `${process.env.ARBISCAN_API_KEY}`,
             b2testnet: `${process.env.B2TEST_NET_API_KEY}`,
+            bevmtestnet: `${process.env.BEVMTEST_NET_API_KEY}`
         },
         customChains: [
             {
@@ -98,6 +113,22 @@ const config: HardhatUserConfig = {
                 urls: {
                     apiURL: "https://bsquared-testnet.l2scan.co/api",
                     browserURL: "https://bsquared-testnet.l2scan.co/"
+                },
+            },
+            {
+                network: "ailayertestnet",
+                chainId: 2648,
+                urls: {
+                    apiURL: "https://testnet-explorer.ailayer.xyz/api",
+                    browserURL: "https://testnet-explorer.ailayer.xyz/"
+                },
+            },
+            {
+                network: "bevmtestnet",
+                chainId: 11503,
+                urls: {
+                    apiURL: "https://scan-testnet-api.bevm.io/api",
+                    browserURL: "https://scan-testnet.bevm.io/"
                 },
             },
         ],
